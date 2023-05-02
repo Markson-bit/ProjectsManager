b9bde140baa7b981790b0afff1c34163de69ff95
Marek Małolepszy <marek4924@gmail.com>
2023-05-02 20:29:51 +0200

    Changed text file to database in SQLite. Rewrited all methods

--- ProjectsManager/pages/ProjectsPage.xaml.cs
+++ ProjectsManager/pages/ProjectsPage.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.IO;
 using ButtonSpace;
 using System.Runtime.Remoting.Contexts;
+using System.Data.SQLite;
 
 namespace ProjectsManager.pages
 {
@@ -24,6 +25,7 @@ namespace ProjectsManager.pages
     public partial class ProjectsPage : Page
     {
         string filePath = AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\\data\testdata.txt";
+        string dataPath = AppDomain.CurrentDomain.BaseDirectory + @"..\\..\\\data\tasksdata.db";
 
         public ProjectsPage()
         {
@@ -31,21 +33,21 @@ namespace ProjectsManager.pages
 
             TaskList.Items.Clear();
 
-            // Reading tasks from file, splitting by ';' char and appending task name to ListBox.
-
-            using (StreamReader sr = new StreamReader(filePath))
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dataPath};Version=3;"))
             {
-                string line;
+                connection.Open();
 
-                while ((line = sr.ReadLine()) != null)
+                SQLiteCommand command = new SQLiteCommand("SELECT * FROM Tasks", connection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
                 {
-                    string[] parts = line.Split(';');
-                    string foundName = parts[0];
-                    TaskList.Items.Add(foundName);
+                    string taskName = reader.GetValue(0).ToString();
+
+                    TaskList.Items.Add(taskName);                 
                 }
+
+                connection.Close();
             }
-
-
         }
 
         private void AddTask_Click(object sender, RoutedEventArgs e)
@@ -61,13 +63,37 @@ namespace ProjectsManager.pages
                     MessageBoxResult result = MessageBox.Show("Exactly same tasks already exist. Are you sure you want to add it again?", "Action needed.", MessageBoxButton.YesNo, MessageBoxImage.Question);
                     if (result == MessageBoxResult.Yes)
                     {
+
+                        string taskName = NameBox.Text;
+                        string taskPerson = WorkerBox.Text;
+                        string taskDesc = DescBox.Text;
+                        string taskDate = DateBox.Text;
+
                         // If yes, adding name to ListBox
                         TaskList.Items.Add(NameBox.Text);
 
-                        string content = NameBox.Text + ";" + WorkerBox.Text + ";" + DescBox.Text + ";" + DateBox.Text + Environment.NewLine;
-                        File.AppendAllText(filePath, content);
+                        using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dataPath};Version=3;"))
+                        {
+                            connection.Open();
+
+                            string sql = "insert into Tasks VALUES(@Name, @Person, @Desc, @Date)";
+
+                            using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                            {
+                                command.Parameters.AddWithValue("@Name", taskName);
+                                command.Parameters.AddWithValue("@Person", taskPerson);
+                                command.Parameters.AddWithValue("@Desc", taskDesc);
+                                command.Parameters.AddWithValue("@Date", taskDate);
+
+                                command.ExecuteNonQuery();
+                            }
+
+                            connection.Close();
+                        }
 
                         NameBox.Clear();
+                        WorkerBox.Clear();
+                        DescBox.Clear();
                     }
 
                     else
@@ -83,9 +109,33 @@ namespace ProjectsManager.pages
                 // If same task does not exist in ListBox
                 else
                 {
+                    string taskName = NameBox.Text;
+                    string taskPerson = WorkerBox.Text;
+                    string taskDesc = DescBox.Text;
+                    string taskDate = DateBox.Text;
+
+                    // If yes, adding name to ListBox
                     TaskList.Items.Add(NameBox.Text);
-                    string content = NameBox.Text + ";" + WorkerBox.Text + ";" + DescBox.Text + ";" + DateBox.Text + Environment.NewLine;
-                    File.AppendAllText(filePath, content);
+
+                    using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dataPath};Version=3;"))
+                    {
+                        connection.Open();
+
+                        string sql = "insert into Tasks VALUES(@Name, @Person, @Desc, @Date)";
+
+                        using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                        {
+                            command.Parameters.AddWithValue("@Name", taskName);
+                            command.Parameters.AddWithValue("@Person", taskPerson);
+                            command.Parameters.AddWithValue("@Desc", taskDesc);
+                            command.Parameters.AddWithValue("@Date", taskDate);
+
+                            command.ExecuteNonQuery();
+                        }
+
+                        connection.Close();
+                    }
+
                     NameBox.Clear();
                     WorkerBox.Clear();
                     DescBox.Clear();
@@ -98,26 +148,36 @@ namespace ProjectsManager.pages
             // Getting name of selected task
             string selectedTask = this.TaskList.SelectedItem.ToString();
 
-            // Reading data from file
-            string[] lines = File.ReadAllLines(filePath);
-            List<string> linesList = lines.ToList();
-
-            // Finding line where name exists
-            string lineToRemove = linesList.FirstOrDefault(line => line.StartsWith(selectedTask + ";"));
-
-            // Deleting specific line from file and overwriting it without deleted line
-            if (lineToRemove != null)
+            // Removing selected task from the database
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dataPath};Version=3;"))
             {
-                linesList.Remove(lineToRemove);
-                File.WriteAllLines(filePath, linesList.ToArray());
+                connection.Open();
+
+                string sql = "DELETE FROM Tasks WHERE Name=@Name";
+
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", selectedTask);
+
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
             }
 
-            // Deleting from ListBox
+            // Removing selected task from ListBox
             if (this.TaskList.SelectedIndex != -1)
             {
                 this.TaskList.Items.RemoveAt(this.TaskList.SelectedIndex);
             }
+
+            // Clearing data from details section after deleting task
+            DetailsName.Text = String.Empty;
+            DetailsWorker.Text = String.Empty;
+            DetailsDesc.Text = String.Empty;
+            DetailsDate.Text = String.Empty;
         }
+
 
 
         // If task inside ListBox is checked, overwriting TextBlocks in Details
@@ -132,33 +192,34 @@ namespace ProjectsManager.pages
                 DetailsDate.Text = String.Empty;
             }
 
-            // Reading file and splitting data by ';' char. Name is content behind first ';' char, worker is after first... - Filling details section
+            // Reading from database by select query and splitting data.
             if (TaskList.SelectedIndex != -1)
             {
-                using (StreamReader sr = new StreamReader(filePath))
+                string selectedTaskName = TaskList.SelectedItem.ToString();
+
+                using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dataPath};Version=3;"))
                 {
-                    for (int i = 0; i <= TaskList.SelectedIndex; i++)
+                    connection.Open();
+
+                    SQLiteCommand command = new SQLiteCommand("SELECT * FROM Tasks WHERE Name=@Name", connection);
+                    command.Parameters.AddWithValue("@Name", selectedTaskName);
+                    SQLiteDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
                     {
-                        string line = sr.ReadLine();
+                        string taskName = reader.GetValue(0).ToString();
+                        string taskPerson = reader.GetValue(1).ToString();
+                        string taskDesc = reader.GetValue(2).ToString();
+                        string taskDate = reader.GetValue(3).ToString();
 
-                        if (i == TaskList.SelectedIndex)
-                        {
-                            string[] parts = line.Split(';');
-                            string foundName = parts[0];
-                            string foundPerson = parts[1];
-                            string foundDesc = parts[2];
-                            string foundDate = parts[3];
-
-                            DetailsName.Text = foundName;
-                            DetailsWorker.Text = foundPerson;
-                            DetailsDesc.Text = foundDesc;
-                            DetailsDate.Text = foundDate;
-                            break;
-                        }
+                        DetailsName.Text = taskName;
+                        DetailsWorker.Text = taskPerson;
+                        DetailsDesc.Text = taskDesc;
+                        DetailsDate.Text = taskDate;
                     }
+
+                    connection.Close();
                 }
-
-
             }
         }
     }
